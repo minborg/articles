e86245fc7a5d3d80be89894d85e2f000cbab6013
Per Minborg <pminborg@gmail.com>
2023-08-01 15:26:38 +0200

    Update

--- 2023/March/22-jep442-FFM-Third-Preview/src/main/java/org/minborg/jep442/Demo.java
+++ 2023/March/22-jep442-FFM-Third-Preview/src/main/java/org/minborg/jep442/Demo.java
@@ -8,42 +8,50 @@ import java.lang.foreign.MemorySegment;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.VarHandle;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
 import static java.lang.foreign.MemoryLayout.PathElement.*;
 import static java.lang.foreign.MemoryLayout.*;
 import static java.lang.foreign.ValueLayout.*;
 
-// JDK 21 builds: https://jdk.java.net/
-// Make sure you set your JAVA_HOME to a JDK 21 build and provide
+// JDK 22 builds: https://jdk.java.net/
+// Make sure you set your JAVA_HOME to a JDK 22 build and provide
 // "--enable-preview" when run
 // In IntelliJ: Add the --enable-preview flag in the "compiler settings"
-// For Maven add: export MAVEN_OPTS="--enable-preview"
+// For Maven add: export MAVEN_OPTS="--enable-preview --enable-native-access=ALL-UNNAMED"
+// java --enable-preview -cp target/jep442-1.0.0-SNAPSHOT.jar org.minborg.jep442.Demo
 public class Demo {
 
     // With "import static" of PathElement.*, MemoryLayout.* and ValueLayout.*
     private static final MemoryLayout POINT = structLayout(
             JAVA_INT.withName("x"),
             JAVA_INT.withName("y")
-    ).withName("point");
+    );
+
+    // struct Point {
+    //   int x;
+    //   int y;
+    // };
 
     // Accessor for x
     private static final VarHandle X = POINT.varHandle(groupElement("x"));
     // Accessor for y
     private static final VarHandle Y = POINT.varHandle(groupElement("y"));
 
-
     public static void main(String[] args) throws Throwable {
 
         {
             // ...
-            MemorySegment segment = Arena.ofAuto().allocate(16);
+            MemorySegment segment = Arena.ofAuto().allocate(POINT);
+            System.out.println("segment.byteSize() = " + segment.byteSize()); // segment.byteSize() = 8
             // ...
         } // Segment eligible for collection by the GC here.
         // Actual time of collection is unspecified.
 
         try (var arena = Arena.ofConfined()) {
-            var segment = arena.allocate(16);
+            var segment = arena.allocate(POINT);
         } // Segment is deterministically freed here
 
         try (var arena = Arena.ofConfined()) {
@@ -51,7 +59,7 @@ public class Demo {
             X.set(point, 3);
             Y.set(point, 4);
             System.out.println(
-                    Arrays.toString(point.toArray(JAVA_INT))
+                    Arrays.toString(point.toArray(JAVA_INT)) // [3, 4]
             );
         } // Point is deterministically freed here
 
@@ -59,21 +67,40 @@ public class Demo {
             var point = new Point(arena);
             point.x(3);
             point.y(4);
-            System.out.println(point);
+            System.out.println(point); // Point[3, 4]
         } // Point is deterministically freed here
+
+        /* Might Preview in 22 ...
+
+        record Point(int x, int y){}
+
+        var segment = MemorySegment.ofArray(new int[]{3, 4, 10, 12});
+
+        Mapper<Point> mapper = POINT.recordMapper(Point.class);
+
+        Point point = mapper.get(segment); // Point[3, 4]
+
+        List<Point> points = segment.elements(POINT)
+                .map(mapper::get)
+                .toList(); // [Point[3, 4], Point[10, 12]]
+
+         record Foo(int y){}
+         Foo foo = POINT.recordMapper(Foo.class).get(segment) // Foo(4)
+
+         */
 
         Linker linker = Linker.nativeLinker();
         MethodHandle strlen = linker.downcallHandle(
-                linker.defaultLookup().find("strlen").get(),
+                linker.defaultLookup().find("strlen").orElseThrow(),
                 FunctionDescriptor.of(JAVA_LONG, ADDRESS)
         );
 
         try (Arena arena = Arena.ofConfined()) {
-            MemorySegment str = arena.allocateUtf8String("Hello");
-            long len = (long) strlen.invoke(str);
-            System.out.println("The length is " + len);
+            var s = "Hello";
+            MemorySegment sSegment = arena.allocateUtf8String(s);
+            long len = (long) strlen.invokeExact(sSegment);
+            System.out.println("The length of '" + s + "' is " + len); // The length of 'Hello' is 5
         }
-
 
     }
 
@@ -111,7 +138,7 @@ public class Demo {
 
         @Override
         public String toString() {
-            return "(" + x() + ", " + y() + ")";
+            return "Point[" + x() + ", " + y() + "]";
         }
 
         @Override
